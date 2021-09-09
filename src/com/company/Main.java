8da57a0b2da9558c9334088dfc858af7f1815e89
Grid112 <sumitware1999@gmail.com>
2021-09-09 23:34:49 +0530

    For Vovels And Consonents

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,43 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number to check even or odd : ");
-        int N = sc.nextInt();
-        if (N % 2 == 0){
-            System.out.println(N + " : is an even number");
-        }
-        else {
-            System.out.println(N + " : is an odd number");
+        char character;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Character : ");
+        character = scanner.next().charAt(0);
+        switch (character) {
+            case 'a':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'e':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'i':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'o':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'u':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'A':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'E':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'I':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'O':
+                System.out.println(character + " : is an Vowel");
+                break;
+            case 'U':
+                System.out.println(character + " + : is an Vowel");
+                break;
+            default:
+                System.out.println(character + " : is an Consonent");
         }
 
     }
