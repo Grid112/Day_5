00dfd55c5dedcc5092f988415d9ad2228a6030f1
Grid112 <sumitware1999@gmail.com>
2021-09-09 23:31:27 +0530

    Even And Odd Number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int A, B, Swap;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of A");
-        A = sc.nextInt();
-        System.out.println("Enter the value of B");
-        B = sc.nextInt();
-        System.out.println("before swapping numbers A: "+A);
-        System.out.println("before swapping numbers B: "+B);
-        Swap = A;
-        A = B;
-        B = Swap;
-        System.out.println("After swapping A: "+A);
-        System.out.println("After swapping B: "+B);
-
+        System.out.print("Enter a number to check even or odd : ");
+        int N = sc.nextInt();
+        if (N % 2 == 0){
+            System.out.println(N + " : is an even number");
+        }
+        else {
+            System.out.println(N + " : is an odd number");
+        }
 
     }
 }
