ece46d892253945c060bb5dd9af3a7cbaee0c331
Grid112 <sumitware1999@gmail.com>
2021-09-09 23:18:09 +0530

    To Get Quotient And Remaindeer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int N;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number ::");
-        N = sc.nextInt();
 
-        for(int i = 2; i< N; i++) {
-            while(N%i == 0) {
-                System.out.println(i+" ");
-                N = N/i;
-            }
-        }
-        if(N >2) {
-            System.out.println(N);
-        }
+        int quotient, remainder, dividend, divisor;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Dividend value : ");
+        dividend = scanner.nextInt();
+
+        System.out.print("Enter Divisor value : ");
+        divisor = scanner.nextInt();
+
+        quotient = dividend / divisor;
+        remainder = dividend % divisor;
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder= " + remainder);
     }
 }
